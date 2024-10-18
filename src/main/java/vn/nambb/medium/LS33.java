c197c9a87a03617b327b77bf4407bedabd2a277f
nambb <nam.buiba@abs.vn>
2024-10-18 10:53:32 +0700

    leetcode 33

--- src/main/java/vn/nambb/medium/LS33.java
+++ src/main/java/vn/nambb/medium/LS33.java
@@ -2,8 +2,8 @@ package vn.nambb.medium;
 
 public class LS33 {
     public static void main(String[] args) {
-        int[] nums = {1, 2, 3, 4, 5, 6};
-        int target = 4;
+        int[] nums = {4, 5, 6, 7, 0, 1, 2};
+        int target = 1;
         System.out.println(search(nums, target));
     }
 
@@ -15,20 +15,23 @@ public class LS33 {
             return 0;
         }
 
-        while (left < right) {
-            if (nums[left] == target) {
-                return left;
-            }
-            if (nums[right] == target) {
-                return right;
-            }
+        while (left <= right) {
             int mid = (left + right) / 2;
-            System.out.println(mid);
-            if (nums[mid] == target) return mid;
-            if (nums[mid] > nums[right]) {
-                left = mid + 1;
+            if (nums[mid] == target) {
+                return mid;
+            }
+            if (nums[left] <= nums[mid]) {
+                if (nums[left] <= target && target < nums[mid]) {
+                    right = mid - 1;
+                } else {
+                    left = mid + 1;
+                }
             } else {
-                right = mid;
+                if (nums[right] >= target && target > nums[mid]) {
+                    left = mid + 1;
+                } else {
+                    right = mid - 1;
+                }
             }
         }
 

d0c336bef15ec6b8298bbc801361aa88b2832a57
nambb <nam.buiba@abs.vn>
2024-11-07 14:01:04 +0700

    leetcode 424

---
+++ src/main/java/vn/nambb/medium/LS424.java
@@ -0,0 +1,27 @@
+package vn.nambb.medium;
+
+public class LS424 {
+    public static void main(String[] args) {
+        String s = "ABAB";
+        int k = 2;
+        LS424 ls424 = new LS424();
+        System.out.println(ls424.characterReplacement(s, k));
+    }
+
+    public int characterReplacement(String s, int k) {
+        int maxCount = 0;
+        int max = 0;
+        int left = 0;
+        int[] charArray = new int[26];
+        for (int right = 0; right < s.length(); right++) {
+            charArray[s.charAt(right) - 'A']++;
+            maxCount = Math.max(maxCount, charArray[s.charAt(right) - 'A']);
+            while (right - left + 1 - maxCount > k) {
+                charArray[s.charAt(left) - 'A']--;
+                left++;
+            }
+            max = Math.max(max, right - left + 1);
+        }
+        return max;
+    }
+}
